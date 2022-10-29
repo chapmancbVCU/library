f37c5d668806c646a7f486c597ee12f4638b1ff0
Chad Chapman <chad.chapman2010@gmail.com>
2022-10-28 21:09:00 -0400

    Added a function and constructor.  Added jsdoc comment to constructor.

--- js/library.js
+++ js/library.js
@@ -2,25 +2,39 @@
  *         Name: calculator.js
  *       Author: Chad Chapman
  * Date Created: October 28, 2022
- *  Description: Implementation for Library
+ *  Description: Functions that support implementation of Library website.
 ******************************************************************************/
 
+/******************************************************************************
+ * GLOBAL VARIABLES
+ *****************************************************************************/
+/**
+ * Array that contains Book objects
+ * @type {Array<Book>}
+ */
+let myLibrary = [];
 
+/**
+ * Constructor for adding a book to the library.
+ * @param {string} title The title of the book to be added to the library 
+ * @param {string} author The author's name
+ * @param {number} pages The number of pages in the book
+ * @param {boolean} read A boolean value used to report if the bood has been
+ * read
+ */
+function Book(title, author, pages, read) {
+
+}
 
 /******************************************************************************
- *        Name: 
- * Description: 
+ *        Name: addBooktoLibrary
+ * Description: Function for adding a book to the library array.
  *   Arguments: 
  *     Returns: 
  *****************************************************************************/
+function addBookToLibrary() {
 
-/******************************************************************************
- *        Name: 
- * Description: 
- *   Arguments: 
- *     Returns: 
- *****************************************************************************/
-
+}
 /******************************************************************************
  *        Name: 
  * Description: 
