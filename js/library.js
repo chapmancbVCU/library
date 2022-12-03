7cbfec2cd7ba968d3dcaeb2bd32836622ae00487
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-02 19:58:13 -0500

    Created Library classes and got cards to be rendered.

--- js/library.js
+++ js/library.js
@@ -35,6 +35,8 @@ class Book {
         }
     }
 
+    
+
     /**
      * This function returns a message depending on if the book has been read yet.
      * @param {boolean} readStatus The status of whether or not the book has 
@@ -52,6 +54,82 @@ class Book {
     }
 }
 
+class Library {
+    constructor() {
+        this.onlineLibrary = [];
+    }
+
+    /**
+     * This function takes a book object and builds the information card.  This 
+     * information is then printed to the browser window.
+     * @param {Book} book A book object whose variables will be used to populate 
+     * the information in a card that appears on the website
+     * @param {number} index The index of the book in the myLibrary array
+     */
+    createCard (book, index) {
+        const card = document.createElement('div');
+        card.classList.add('book-info');
+        card.setAttribute('id', `${index}`);
+        container.append(card);
+
+        const bookTitle = document.createElement('p');
+        bookTitle.classList.add('book-title');
+        bookTitle.textContent = book.title;
+        card.appendChild(bookTitle);
+
+        const border = document.createElement('hr');
+        border.classList.add('card-border');
+        card.appendChild(border);
+
+        const authorName = document.createElement('p');
+        authorName.classList.add('author-name');
+        authorName.textContent = `By: ${book.author}`;
+        card.appendChild(authorName);
+
+        const numberOfPages = document.createElement('p');
+        numberOfPages.classList.add('number-of-pages');
+        numberOfPages.textContent = `Length: ${book.pages} pages`;
+        card.appendChild(numberOfPages);
+
+        const readStatus = document.createElement('p');
+        readStatus.classList.add('is-book-read');
+        readStatus.textContent = book.isReadMessage(book.read);
+        card.appendChild(readStatus);
+        
+        const cardButtons = document.createElement('div');
+        cardButtons.classList.add('card-buttons');
+        card.appendChild(cardButtons);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('card-button');
+        deleteButton.textContent = "Delete";
+        cardButtons.appendChild(deleteButton);
+
+        const changeReadStatus = document.createElement('button');
+        changeReadStatus.classList.add('card-button');
+        changeReadStatus.textContent = "Toggle Read";
+        cardButtons.appendChild(changeReadStatus);
+
+        // Each card needs an event listener for its delete button.
+        deleteButton.addEventListener('click', () => {
+            deleteCard(index, card);
+        });
+
+        // Each card needs an event listener for its read button.
+        changeReadStatus.addEventListener('click', () => {
+            updateReadStatus(index, readStatus);
+        });
+    }
+    /**
+     * This function takes the myLibrary array and builds all of the individual 
+     * information cards shown on the webpage.
+     */
+    displayCards() {
+        for(let i = 0; i < this.onlineLibrary.length; i++) {
+            this.createCard(this.onlineLibrary[i], i);
+        }
+    }
+}
 /******************************************************************************
  * Test Books
  *****************************************************************************/
@@ -68,3 +146,27 @@ const bookFive = new Book("Physics for Scientists and Engineers with Modern Phys
                     "Serway", 1442, false);
 const bookSix = new Book("HTML & CSS: design and build websites",
                     "John Duckett", 490, true);
+
+/******************************************************************************
+ * GLOBAL VARIABLES
+ *****************************************************************************/
+/**
+ * Array that contains Book objects
+ * @type {Array<Book>}
+ */
+let myLibrary = new Library();
+
+myLibrary.onlineLibrary.push(bookZero);
+myLibrary.onlineLibrary.push(bookOne);
+myLibrary.onlineLibrary.push(bookTwo);
+myLibrary.onlineLibrary.push(bookThree);
+myLibrary.onlineLibrary.push(bookFour);
+myLibrary.onlineLibrary.push(bookFive);
+myLibrary.onlineLibrary.push(bookSix);
+
+const container = document.querySelector('#container');
+
+/******************************************************************************
+ * Function call upon page load
+ *****************************************************************************/
+myLibrary.displayCards();
